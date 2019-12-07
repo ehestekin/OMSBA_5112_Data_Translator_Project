f98255e07d517244635ef9f93b261ea05efbd631
Eric Hestekin <ehestekin@seattleu.edu>
2019-12-06 19:42:03 -0800

    Switching to new dataset

--- R_Code/ghana_ag_data_analysis.R
+++ R_Code/ghana_ag_data_analysis.R
@@ -23,31 +23,27 @@
 library(tidyverse)
 library(haven)
 
-#some issues with data need stata 13 read function
-#install.packages('readstata13')
-
-library(readstata13)
 
 #array of all .dta files in dataset (not using this section)----
-file_list <- list.files('Raw_Data/Unzipped Data/', pattern = '\\.dta*', recursive = TRUE)
+file_list <- list.files('Raw_Data/glss4_new/', pattern = '\\.dta*', recursive = TRUE)
 
 #read in file from list
-test <- read_dta(paste('Raw_Data/Unzipped Data/' , file_list[1], sep = ''))
+test <- read_dta(paste('Raw_Data/glss4_new/' , file_list[1], sep = ''))
 
 #household and location info------------------------------------
-id_df         <- read.dta13('Raw_Data/Unzipped Data/sec0a.dta')
-education_df  <- read.dta13('Raw_Data/Unzipped Data/sec2a.dta')
-land_chars_df <- read.dta13('Raw_Data/Unzipped Data/sec8a1.dta')
+id_df         <- read_dta('Raw_Data/glss4_new/sec0a.dta')
+education_df  <- read_dta('Raw_Data/glss4_new/sec2a.dta')
+land_chars_df <- read_dta('Raw_Data/glss4_new/sec8a1.dta')
 
 #financials-----------------------------------------------------
-ag_profit_df    <- read.dta13('Raw_Data/Unzipped Data/aggregates/agg2.dta')
-costs_land_df   <- read.dta13('Raw_Data/Unzipped Data/aggregates/exp3.dta')
-costs_crops_df  <- read.dta13('Raw_Data/Unzipped Data/aggregates/exp4.dta')
-costs_livestock <- read.dta13('Raw_Data/Unzipped Data/aggregates/exp5.dta')
+ag_profit_df    <- read_dta('Raw_Data/glss4_new/aggregates/agg2.dta')
+costs_land_df   <- read_dta('Raw_Data/glss4_new/aggregates/exp3.dta')
+costs_crops_df  <- read_dta('Raw_Data/glss4_new/aggregates/exp4.dta')
+costs_livestock <- read_dta('Raw_Data/glss4_new/aggregates/exp5.dta')
 
 #community info-------------------------------------------------
-community_df        <- read.dta13('Raw_Data/Unzipped Data/community/cs2.dta')
-community_health_df <- read.dta13('Raw_Data/Unzipped Data/community/cs4b.dta')
-community_ag_df     <- read.dta13('Raw_Data/Unzipped Data/community/cs5b.dta')
+community_df        <- read_dta('Raw_Data/glss4_new/community/cs2.dta')
+community_health_df <- read_dta('Raw_Data/glss4_new/community/cs4b.dta')
+community_ag_df     <- read_dta('Raw_Data/glss4_new/community/cs5b.dta')
 
 
